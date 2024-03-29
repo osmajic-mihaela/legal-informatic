765fe12053ea944533f60e90db33cce7c543a7da
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2024-03-29 11:00:25 +0100

    Add repo for dr-device legal case

---
+++ legal-informatic-backend/src/main/java/org/example/legalinformaticbackend/repository/DrDeviceLegalCaseRepository.java
@@ -0,0 +1,8 @@
+package org.example.legalinformaticbackend.repository;
+
+import org.example.legalinformaticbackend.model.DrDeviceLegalCase;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface DrDeviceLegalCaseRepository extends  DbEntityRepository<DrDeviceLegalCase> {
+}
